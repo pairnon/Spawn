14e6131f1c4c3e756b6e61269e1b06efd4c98c8a
pairnon <janpairnon@gmail.com>
2024-06-14 01:31:21 -0700

    Add plugin instance getter

--- app/src/main/java/io/papermc/spawn/Main.java
+++ app/src/main/java/io/papermc/spawn/Main.java
@@ -1,8 +1,21 @@
 package io.papermc.spawn;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
+
+    private static Main plugin;
+
+    public static Main getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        plugin = this;
+    }
     
 }
