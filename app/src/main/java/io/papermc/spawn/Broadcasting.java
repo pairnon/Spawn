a7e9a2f364551cd97a1e6f2fe3333bb1afe8b8ef
pairnon <janpairnon@gmail.com>
2024-06-15 13:32:50 -0700

    Add sendAlert method

--- app/src/main/java/io/papermc/spawn/Broadcasting.java
+++ app/src/main/java/io/papermc/spawn/Broadcasting.java
@@ -1,11 +1,16 @@
 package io.papermc.spawn;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 public class Broadcasting {
+
+    public static void sendAlert(Player p, String message) {
+        p.sendMessage(Component.text("[ALERT] " + message, NamedTextColor.RED));
+    }
 
     public static void sendMessageResponse(CommandSender c, String message) {
         c.sendMessage(Component.text(message, NamedTextColor.GOLD));
