7b516d9e98e11ee32f585c92fe5510e46b11a396
pairnon <janpairnon@gmail.com>
2024-06-16 16:27:27 -0700

    Add removewarnradius command

--- app/src/main/java/io/papermc/spawn/Main.java
+++ app/src/main/java/io/papermc/spawn/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.papermc.spawn.commands.CommandRemoveWarnRadius;
 import io.papermc.spawn.commands.CommandSetSpawn;
 import io.papermc.spawn.commands.CommandSetWarnRadius;
 import io.papermc.spawn.commands.CommandSpawn;
@@ -28,6 +29,7 @@ public class Main extends JavaPlugin implements Listener {
 
         this.getCommand("setspawn").setExecutor(new CommandSetSpawn());
         this.getCommand("setwarnradius").setExecutor(new CommandSetWarnRadius());
+        this.getCommand("removewarnradius").setExecutor(new CommandRemoveWarnRadius());
         this.getCommand("spawn").setExecutor(new CommandSpawn());
     }
     
---
+++ app/src/main/java/io/papermc/spawn/commands/CommandRemoveWarnRadius.java
@@ -0,0 +1,40 @@
+package io.papermc.spawn.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import io.papermc.spawn.Broadcasting;
+import io.papermc.spawn.Main;
+
+public class CommandRemoveWarnRadius implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!sender.hasPermission("minecraft.op")) {
+            Broadcasting.sendErrorResponse(sender, "You do not have access to this command.");
+            return true;
+        }
+
+        World world = Bukkit.getWorld("world");
+
+        PersistentDataContainer pdc = world.getPersistentDataContainer();
+
+        if (!pdc.has(new NamespacedKey(Main.getPlugin(), "warningRadius"), PersistentDataType.INTEGER)) {
+            Broadcasting.sendErrorResponse(sender, "The warning radius has not been set.");
+            return true;
+        }
+
+        int warningRadius = pdc.get(new NamespacedKey(Main.getPlugin(), "warningRadius"), PersistentDataType.INTEGER);
+        pdc.remove(new NamespacedKey(Main.getPlugin(), "warningRadius"));
+        Broadcasting.sendMessageResponse(sender, "The warning radius of size " + warningRadius + " has been removed.");
+
+        return true;
+    }
+}
