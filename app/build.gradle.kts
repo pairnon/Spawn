9f71bd2b5f6a0838ed95c44169f687550f5311c5
pairnon <janpairnon@gmail.com>
2024-07-03 14:42:07 -0700

    chore: bump mc / api version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
 
-    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
 }
 
 application {
@@ -48,7 +48,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.20.6")
+        minecraftVersion("1.21")
     }
 }
 
